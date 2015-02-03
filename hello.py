2ab134ffc95eecc5e4f671c5e0b5b9e6e821f0a3
varsis <chris@varsisstudio.com>
2015-02-03 09:56:42 -0700

    Initial lab

---
+++ hello.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import sqlite3
+from flask import Flask,render_template, redirect,url_for,request
+#conn = sqlite3.connect('pylab4.db')
+
+app = Flask(__name__)
+conn = None
+
+def get_conn():
+	global conn
+	if conn is None:
+		conn = sqlite3.connect('pylab4.db')
+		conn.row_factory = sqlite3.Row
+	return conn
+
+def close_connection():
+	global conn
+	if conn is not None:
+		conn.close()
+		conn = None
+
+def query_db(query, args=(), one=False):
+	cur = get_conn().cursor()
+	cur.execute(query,args)
+	r = cur.fetchall()
+	cur.close()
+	return (r[0] if r else None) if one else r
+
+def add_task(task):
+	query_db("insert into tasks(category,priority,description) values(?,?,?)",task, one=True)
+	get_conn().commit();
+	
+def cols():
+	return ["category","priority","description"]
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/tasks',methods=["GET","POST"])
+def tasks(name=None):
+		#post
+	if request.method == "POST":
+		form = request.form
+		task = (form['category'],form['priority'],form['description'])	
+		add_task(task)
+		return redirect(url_for('tasks'))
+
+	#get 
+	elif request.method == "GET":
+		tasks = query_db("select * from tasks order by priority DESC");
+		return render_template('main.html', name=name, cols=cols(),tasks=tasks)
+
+'''@app.route('/task1',methods=["GET","POST"])
+def task():
+	#post
+	if request.method == "POST":
+		category = request.form['category']
+		tasks.append({'category': category})
+		return redirect(url_for('task'))
+
+	#get 
+	elif request.method == "GET":
+		cols = ["category","priority","description"]
+		return render_template('main.html',cols=cols())
+'''		
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
